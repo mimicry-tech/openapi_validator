16c48f7f88844f957d27682ca1f2f006e246b93c
Florian Kraft <schnuffifk@gmail.com>
2021-07-13 21:25:15 +0200

    initial commit
    starts fresh

---
+++ lib/openapi_validator.ex
@@ -0,0 +1,18 @@
+defmodule OpenapiValidator do
+  @moduledoc """
+  Documentation for `OpenapiValidator`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> OpenapiValidator.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/openapi_validator_test.exs
@@ -0,0 +1,8 @@
+defmodule OpenapiValidatorTest do
+  use ExUnit.Case
+  doctest OpenapiValidator
+
+  test "greets the world" do
+    assert OpenapiValidator.hello() == :world
+  end
+end
