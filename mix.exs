3a378773a62c82d0bb289a7d3d30dba7255af2b5
Florian Kraft <schnuffifk@gmail.com>
2022-01-09 08:45:13 +0100

    supports 1.13

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule OpenAPIv3Validator.MixProject do
     [
       app: :openapiv3_validator,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.1",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       pacakge: package(),
